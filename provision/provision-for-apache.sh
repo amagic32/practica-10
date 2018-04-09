f79ba912604eda2050b1090445b224423551c796
amagic32 <tinoxwaadri@gmail.com>
2018-04-09 14:43:38 +0200

    cambios practica-10

--- provision/provision-for-apache.sh
+++ provision/provision-for-apache.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+#INSTALACIÓN APACHE
 apt-get update
 apt-get upgrade
 apt-get install -y apache2
 apt-get install -y php libapache2-mod-php php-mcrypt php-mysql
+/etc/init.d/apache2 restart
+
+#INSTALACIÓN MYSQL CLIENTE
+apt-get -y install debconf-utils
+
+DB_ROOT_PASSWD=root
+
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DB_ROOT_PASSWD"
+
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DB_ROOT_PASSWD"
+
+apt-get install -y mysql-server
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+/etc/init.d/mysql restart
--- provision/provision-for-balanceador.sh
+++ provision/provision-for-balanceador.sh
@@ -1,6 +1,20 @@
-#!/bin/bashapt-get update
+#!/bin/bash
 
+#INSTALACIÓN APACHE
 apt-get update
 apt-get upgrade
 apt-get install -y apache2
+apt-get install -y php libapache2-mod-php php-mcrypt php-mysql
+/etc/init.d/apache2 restart
 
+#INSTALACIÓN DE MÓDULOS
+a2enmod proxy
+a2enmod proxy_http
+a2enmod proxy_ajp
+a2enmod rewrite
+a2enmod deflate
+a2enmod headers
+a2enmod proxy_balancer
+a2enmod proxy_connect
+a2enmod proxy_html
+a2enmod lbmethod_byrequests
